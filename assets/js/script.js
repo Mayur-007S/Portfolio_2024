e3f2dab9dfc820966f6aca727284815366490e76
Mayur Myana <mayurmyana111@gmail.com>
2024-10-31 12:53:54 +0530

    Add comment

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,6 +9,6 @@ const burger = document.getElementById('burger');
 function show() {
     return document.getElementById('nav-list').style = "opacity: 1; transition:  250ms ease-in-out; display: flex; flex-direction: column; position: absolute; background-color: #Def2f1;";
 }
-function disable() {
-    return document.getElementById('nav-list').style = "opacity: 0; transition: opacity 250ms ease-out";
-}
+// function disable() {
+//     return document.getElementById('nav-list').style = "opacity: 0; transition: opacity 250ms ease-out";
+// }
